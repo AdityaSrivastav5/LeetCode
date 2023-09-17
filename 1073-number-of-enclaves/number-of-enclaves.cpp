b7ba291ea6998d7ce4edfc8a69e4dbcd65ecec83
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-09-17 15:21:30 +0530

    Time: 55 ms (71.83%) | Memory: 25.1 MB (95.81%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                         q.push({i,j});
+                         grid[i][j]=0;
+                    }
+                }
+            }
+        }
+        int delrow[4]={-1,0,+1,0};
+        int delcol[4]={0,+1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row + delrow[i];
+                int ncol=col + delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
+                grid[nrow][ncol]==1){
+                    q.push({nrow,ncol});
+                    grid[nrow][ncol]=0;
+                }
+            }
+        }
+        int count=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                    if(grid[i][j]==1){
+                         count++;
+                    }
+            }
+        }
+        return count;
+    }
+};
