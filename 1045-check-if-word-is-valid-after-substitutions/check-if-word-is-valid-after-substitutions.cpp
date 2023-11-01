5400a99355bbd7e8b85c161c6bb6f1db54783faf
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-11-02 00:48:16 +0530

    Time: 27 ms (29.78%) | Memory: 8.6 MB (74.18%) - LeetSync

---
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == 'c') {
+                if (st.size() < 2 || st.top() != 'b') return false;
+                st.pop();
+                if (st.top() != 'a') return false;
+                st.pop();
+            } else st.push(c);
+        }
+        return st.empty();
+    }
+};
