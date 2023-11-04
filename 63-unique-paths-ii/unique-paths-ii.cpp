62236879d5562e82cb9783622f293e4aa434a36f
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-11-04 16:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (36.49%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,20 +5,22 @@ public:
         int m = a[0].size();
         if(n==0) return 0;
         if(a[0][0]) return 0;
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
         for(int i=0;i<n;i++){
+            vector<int>cur(m,0);
             for(int j=0;j<m;j++){
-                if(i==0 && j==0)dp[i][j]=1;
-                else if(a[i][j]==1) dp[i][j]=0;
+                if(i==0 && j==0)cur[j]=1;
+                else if(a[i][j]==1) cur[j]=0;
                 else{
                     int up=0,left=0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j]= up +left; 
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j]= up +left; 
                 }
                 
-            }
+            }           
+            prev = cur;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
